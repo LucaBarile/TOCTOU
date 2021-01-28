d08691a577d2022c936d138e82645be106701d11
Luca Barile <74877659+LucaBarile@users.noreply.github.com>
2021-01-28 19:24:44 +0100

    Add files via upload

---
+++ UpdateDll/UpdateDll/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace TOCTOU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string validSignature = "X6hsu623JHDsjxks893y98hdsjkh983hklsjjSJAiu";
+            string signature;
+
+            StreamReader file = new StreamReader(@"dir1\signedDll.dll");
+            signature = file.ReadLine();
+            file.Close();
+
+            if (signature == validSignature)
+                File.Copy(@"dir1\signedDll.dll", @"dir2\signedDll.dll", true);
+        }
+    }
+}
