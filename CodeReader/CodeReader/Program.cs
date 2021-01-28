7138136854903fb57007d096b6351b4b4638317b
Luca Barile <74877659+LucaBarile@users.noreply.github.com>
2021-01-28 19:26:55 +0100

    Add files via upload

---
+++ CodeReader/CodeReader/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstLinef1, firstLinef2, secretCode="";
+            StreamReader file;
+
+            // --- Time Of Check (TOC) ----
+            file = new StreamReader(@"f1.txt");
+            firstLinef1 = file.ReadLine();
+            file.Close();
+            // ----------------------------
+
+            // --- Code between TOC&TOU ---
+            file = new StreamReader(@"f2.txt");
+            firstLinef2 = file.ReadLine();
+            file.Close();
+            // ...
+            // ----------------------------
+
+            // --- Time Of Use (TOU) ------
+            if(firstLinef1.Equals(firstLinef2))
+            {
+                file = new StreamReader(@"f1.txt");
+                file.ReadLine();
+                secretCode = file.ReadLine();
+                file.Close();
+            }
+            // ----------------------------
+
+            Console.WriteLine("The secret code is: " + secretCode);
+
+            Console.ReadLine();
+        }
+    }
+}
